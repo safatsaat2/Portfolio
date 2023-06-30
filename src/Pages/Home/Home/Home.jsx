2b42b10039759db84235566072421f0f88bf455c
Sakib Rahman Safat <sakibsafat47@gmail.com>
2023-06-30 10:37:51 -0700

    Added: Route of Home, Updated: Eslint, Tailwind CSS

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+const Home = () => {
+    return (
+        <div className="text-2xl">
+            <h1 className="text-xl font-header">Hi</h1>
+            <h1 className="text-xl font-para">ehi</h1>
+        </div>
+    );
+};
+
+export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      'header': ['League Spartan', 'sans-serif'],
+      'para': ['Roboto', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [require("daisyui")],
