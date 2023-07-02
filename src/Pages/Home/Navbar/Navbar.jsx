33ed0d5492206d8beb1ae1690c5534bec05f1cde
Sakib Rahman Safat <sakibsafat47@gmail.com>
2023-07-02 00:51:02 -0700

    Added: Navbar

--- src/Pages/Home/Navbar/Navbar.jsx
+++ src/Pages/Home/Navbar/Navbar.jsx
@@ -10,30 +10,68 @@ const Navbar = () => {
   return (
     <div className="fixed w-full bg-word bg-opacity-30">
       <div className="max-w-[1280px] flex items-center mx-auto py-2">
-        <div onClick={() => setMenu(!menu)} className="lg:hidden">
+        <div onClick={() => setMenu(!menu)} className="lg:hidden ml-2">
           {menu ? (
             <Icon icon="ci:hamburger-lg" />
           ) : (
             <Icon icon="akar-icons:cross" />
           )}
         </div>
-        <h2 className="font-header text-word">Safat</h2>
+
+        <h2 className="font-header justify-center ml-5 lg:ml-0 text-word">Safat</h2>
+
+        <div className="flex hidden lg:block mx-auto max-w-[1280px]">
+          <ul className="flex font-header font-semibold text-word text-base">
+            <li className="ml-5 cursor-pointer hover:font-bold hover:bg-word hover:text-bg transition-all duration-500 px-3 py-3 rounded-md">
+              Home
+            </li>
+            <li className="ml-5 cursor-pointer hover:font-bold hover:bg-word hover:text-bg transition-all duration-500 px-3 py-3 rounded-md">
+              About Me
+            </li>
+            <li className="ml-5 cursor-pointer hover:font-bold hover:bg-word hover:text-bg transition-all duration-500 px-3 py-3 rounded-md">
+              Skills
+            </li>
+            <li className="ml-5 cursor-pointer hover:font-bold hover:bg-word hover:text-bg transition-all duration-500 px-3 py-3 rounded-md">
+              Projects
+            </li>
+            <li className="ml-5 cursor-pointer hover:font-bold hover:bg-word hover:text-bg transition-all duration-500 px-3 py-3 rounded-md">
+              Contacts
+            </li>
+          </ul>
+        </div>
+
+        <button className="text-xl px-3 py-1 rounded bg-word text-bg font-medium hover:text-word hover:bg-bg transition-all duration-500">
+          Hire Now
+        </button>
       </div>
       <div
         className={
           menu
             ? "bg-word transition-all -translate-x-[1000px] duration-500 w-1/2 "
-            : "bg-word transition-all  duration-500 h-screen w-1/2 rounded-md"
+            : "bg-word transition-all  duration-500 h-screen w-1/3 rounded-md lg:hidden"
         }
       >
         {menu ? (
           <></>
         ) : (
           <div>
-            <li>hi</li>
-            <li>hi</li>
-            <li>hi</li>
-            <li>hi</li>
+            <ul className="font-header font-semibold text-bg text-base">
+              <li className=" cursor-pointer hover:font-bold hover:bg-bg w-full hover:text-word px-3 py-3 rounded-md">
+                Home
+              </li>
+              <li className=" cursor-pointer hover:font-bold hover:bg-bg w-full hover:text-word px-3 py-3 rounded-md">
+                About Me
+              </li>
+              <li className=" cursor-pointer hover:font-bold hover:bg-bg w-full hover:text-word px-3 py-3 rounded-md">
+                Skills
+              </li>
+              <li className=" cursor-pointer hover:font-bold hover:bg-bg w-full hover:text-word px-3 py-3 rounded-md">
+                Projects
+              </li>
+              <li className=" cursor-pointer hover:font-bold hover:bg-bg w-full hover:text-word px-3 py-3 rounded-md">
+                Contacts
+              </li>
+            </ul>
           </div>
         )}
       </div>
